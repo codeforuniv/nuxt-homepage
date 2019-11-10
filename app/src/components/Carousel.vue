b77f8c88a8fe748c5c90b4458f6b3dcdd8b06ef0
ttmiura <ttmiura225@gmail.com>
2019-11-11 01:26:02 +0900

    とりあえずcarouselを入れてみた (#18)

---
+++ app/src/components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-carousel
+    height="auto"
+    cycle
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+    >
+      <img
+        :src="item.src"
+        width="800px"
+        height="auto"
+      >
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: '/event1.jpg'
+        },
+        {
+          src: '/event2.jpg'
+        },
+        {
+          src: '/event3.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.v-carousel
+  width: 100%
+</style>
--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,92 +1,55 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <Carousel />
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <img
+        src="/logo.png"
+        width="200px"
+      >
+      <h1 class="tagline">
+        大学 × 市民社会 × テクノロジーは未来を変える
+      </h1>
+      <blockquote class="blockquote">
+        Code for Univ.(University)は大学に対してみなさんが抱いている想いを、 想いのまま終わらせずITやネットワーキングを通じて実際の課題解決にコミットするためのシビックテックコミュニティです。
+      </blockquote>
+    </v-row>
+    <v-row class="text-center">
+      <a href="https://twitter.com/codeforuniv" target="_blank" class="sns-link">
+        <i class="fab twitter-icon" />
+      </a>
+      <iframe
+        src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FCode-for-University-103516374334392&width=450&layout=standard&action=like&size=large&show_faces=true&share=true&height=80&appId"
+        width="450"
+        height="80"
+        style="border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
+      />
+      <footer>
+        <div>©️ 2019 Code for University</div>
+      </footer>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Carousel from '@/components/Carousel.vue'
 
 export default {
+  name: 'Home',
   components: {
-    Logo,
-    VuetifyLogo
+    Carousel
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.tagline
+  padding: 20px
+</style>
