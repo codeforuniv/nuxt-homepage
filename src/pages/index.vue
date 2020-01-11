961c5eb9aff179e4b76899578b260d4101f8f3ad
Takahiro Miura <ttmiura225@gmail.com>
2020-01-11 23:32:49 +0900

    index.vueをリファクタ

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,80 +1,83 @@
 <template>
-  <div>
-    <section>
-      <v-layout class="pa-0">
+  <v-container>
+    <v-row>
+      <v-col>
         <Carousel />
-      </v-layout>
-    </section>
-    <section>
-      <v-layout column wrap class="my-12" align-center>
-        <v-flex xs12 sm4 class="my-4">
-          <div class="text-center">
-            <h2 class="headline">
-              大学の3つの役割
-            </h2>
-          </div>
-        </v-flex>
-        <v-flex xs12>
-          <v-container grid-list-xl>
-            <v-layout row wrap align-center>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-palette
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-center">
-                      研究
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    世界の知識の最前線で試行錯誤を重ねながら誰も成し遂げていない発見を目指します。
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-flash
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline">
-                      教育
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    専門的教育の他にも、科学リテラシーや深い洞察など教育は幅広い分野にわたります。
-                    学生だけでなく、知を生み出す場所として意義のある教育を目指します。
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-wrench
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-center">
-                      社会貢献
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    生み出された知識や知恵を使って社会課題を解決します。
-                    行政や企業を巻き込んだ社会的意義のある大学を目指します。
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-flex>
-      </v-layout>
-    </section>
+      </v-col>
+    </v-row>
+    <v-row class="my-12">
+      <v-col class="text-center">
+        <h2> 大学の3つの役割 </h2>
+      </v-col>
+    </v-row>
+    <v-row class="my-12">
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-palette
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline text-center">
+              研究
+            </div>
+          </v-card-title>
+          <v-card-text>
+            世界の知識の最前線で試行錯誤を重ねながら誰も成し遂げていない発見を目指します。
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-flash
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline text-center">
+              教育
+            </div>
+          </v-card-title>
+          <v-card-text>
+            専門的教育の他にも、科学リテラシーや深い洞察など教育は幅広い分野にわたります。
+            学生だけでなく、知を生み出す場所として意義のある教育を目指します。
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-wrench
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline text-center">
+              社会貢献
+            </div>
+          </v-card-title>
+          <v-card-text>
+            生み出された知識や知恵を使って社会課題を解決します。
+            行政や企業を巻き込んだ社会的意義のある大学を目指します。
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
     <section>
       <v-layout column wrap class="my-12" align-center>
         <v-flex xs12 sm4 class="my-4">
@@ -101,218 +104,102 @@
         </v-flex>
       </v-layout>
     </section>
-    <section>
-      <v-layout column wrap class="my-12" align-center>
-        <v-flex xs12 sm4 class="my-4">
-          <div class="text-center">
-            <h2 class="headline">
-              Code for Universityの取り組み
-            </h2>
-          </div>
-        </v-flex>
-        <v-flex xs12>
-          <v-container grid-list-xl>
-            <v-layout row wrap align-center>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-palette
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-center">
-                      アプリ開発支援
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    大学の「研究・教育・社会貢献」を支援するアプリケーションの開発を行っています。
-                    個人開発やハッカソンを通じて、皆さんの思いをアプリにして実装しています。
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-flash
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline">
-                      イベント開催
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    大学×シビックテックの実現する形として、イベントを開催しています。
-                    院生や研究者、行政の方などその時々でフォーカスを変えながら、大学のより良い活用方法について議論しています。
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <v-icon x-large class="blue--text text--lighten-2">
-                      mdi-wrench
-                    </v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-center">
-                      Facebook ページ
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    Code for
-                    Universityの活動やイベントを載せているFacebookページ。
-                    活動に興味のある方・アプリの宣伝・イベントについての質問などなどお気軽にご連絡ください！！
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-flex>
-      </v-layout>
-    </section>
-    <section>
-      <v-layout column wrap class="my-12" align-center>
-        <v-flex xs12 sm4 class="my-4">
-          <div class="text-center">
-            <h2 class="headline">
-              メッセージ
-            </h2>
-          </div>
-        </v-flex>
-        <v-flex xs12>
-          <v-container grid-list-xl>
-            <v-layout row wrap align-center>
-              <v-flex xs12>
-                大学の目的には「研究・教育・社会貢献」の3つがあると言われています。<br>
-                しかし、高速で変化する世界の中で今の大学はこの3つの目的を果たすために様々な問題に直面しています。<br>
-                1つ1つの課題はとても大きく、1人の力では解決できないかもしれません。<br>
-                Code for
-                Universityは、そんな大学のために活動するすべての人を応援します。<br>
-                1人の力では解決できない課題も、誰かが動き始めることで繋がり、社会に広がっていきます。<br>
-                今あなたの頭の中にある思いを、Code for
-                Universityと一緒に形にしてみませんか？<br><br>
-                Code for University代表 三浦 崇寛<br>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-flex>
-      </v-layout>
-    </section>
-    <!-- <v-container> -->
-    <!-- <v-row>
-          <p class="display-1 font-weight-black red--text">
-            RECENT NEWS
-          </p>
-        </v-row> -->
-    <!-- <v-row class="pa-5">
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-        </v-row> -->
-    <!-- </v-container> -->
-    <!-- <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          MESSAGE
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8" class="pa-5 text">
-          大学の目的には「研究・教育・社会貢献」の3つがあると言われています。<br>
-          しかし、高速で変化する世界の中で今の大学はこの3つの目的を果たすために様々な問題に直面しています。1つ1つの課題はとても大きく、1人の力では解決できないかもしれません。<br>
-          Code for Universityは、そんな大学のために活動するすべての人を応援します。<br>
-          1人の力では解決できない課題も、誰かが動き始めることで繋がり、社会に広がっていきます。<br>
-          今あなたの頭の中にある思いを、Code for Universityと一緒に形にしてみませんか？<br><br>
-          <v-row>
-            <v-col cols="12" align="right">
-              Code for University代表 三浦 崇寛
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container> -->
-    <!-- <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          WHAT WE DO
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8">
-
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container> -->
-    <!-- <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          FOR PEOPLE
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8">
-
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          FOR STUDENTS
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8">
-
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          FOR UNIVERSITY
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8">
-
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container>
-      <v-row>
-        <p class="display-1 font-weight-black red--text">
-          FOR GOVERNMENT
-        </p>
-      </v-row>
-      <v-row class="blue-grey lighten-5 pa-5">
-        <v-col cols="8">
-
-        </v-col>
-        <v-col cols="4">
-          asdf
-        </v-col>
-      </v-row>
-    </v-container> -->
-    <v-row justify="center">
+    <v-row class="my-12">
+      <v-col class="text-center">
+        <h2> Code for Universityの主な活動 </h2>
+      </v-col>
+    </v-row>
+    <v-row class="my-12">
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-palette
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline text-center">
+              アプリ開発支援
+            </div>
+          </v-card-title>
+          <v-card-text>
+            大学の「研究・教育・社会貢献」を支援するアプリケーションの開発を行っています。
+            個人開発やハッカソンを通じて、皆さんの思いをアプリにして実装しています。
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-flash
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline">
+              イベント開催
+            </div>
+          </v-card-title>
+          <v-card-text>
+            大学×シビックテックの実現する形として、イベントを開催しています。
+            院生や研究者、行政の方などその時々でフォーカスを変えながら、大学のより良い活用方法について議論しています。
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        class="my-4"
+      >
+        <v-card flat class="transparent">
+          <v-card-text class="text-center">
+            <v-icon x-large class="blue--text text--lighten-2">
+              mdi-wrench
+            </v-icon>
+          </v-card-text>
+          <v-card-title primary-title class="layout justify-center">
+            <div class="headline text-center">
+              Facebook ページ
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Code for
+            Universityの活動やイベントを載せているFacebookページ。
+            活動に興味のある方・アプリの宣伝・イベントについての質問などなどお気軽にご連絡ください！！
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row class="my-12">
+      <v-col class="text-center">
+        <h2> メッセージ </h2>
+      </v-col>
+    </v-row>
+    <v-row class="my-12">
+      <v-col
+        class="text-center"
+      >
+        大学の目的には「研究・教育・社会貢献」の3つがあると言われています。<br>
+        しかし、高速で変化する世界の中で今の大学はこの3つの目的を果たすために様々な問題に直面しています。<br>
+        1つ1つの課題はとても大きく、1人の力では解決できないかもしれません。<br>
+        Code for
+        Universityは、そんな大学のために活動するすべての人を応援します。<br>
+        1人の力では解決できない課題も、誰かが動き始めることで繋がり、社会に広がっていきます。<br>
+        今あなたの頭の中にある思いを、Code for
+        Universityと一緒に形にしてみませんか？<br><br>
+        Code for University代表 三浦 崇寛<br>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center">
       <a
         href="https://twitter.com/codeforuniv"
         target="_blank"
@@ -331,7 +218,7 @@
         allow="encrypted-media"
       />
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
