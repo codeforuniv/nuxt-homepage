399ad8ad44e37ae58da1fae543b2c7ff79bb4b0a
Takahiro Miura <ttmiura225@gmail.com>
2020-01-12 14:58:23 +0900

    NewsCardの見た目を変更

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,32 +1,40 @@
 <template>
-  <v-card
-    class="mx-auto"
-  >
-    <v-img
-      :src="post.images[0]"
-      class="white--text align-end"
-      height="200px"
+  <v-hover v-slot:default="{ hover }">
+    <v-card
+      class="mx-auto"
+      :elevation="hover ? 12 : 2"
     >
-      <v-card-title>{{ post.title }}</v-card-title>
-    </v-img>
+      <v-img
+        :src="post.images[0]"
+        class="white--text align-end"
+        height="200px"
+      >
+        <div
+          class="news-title"
+          style="height: 30%"
+        >
+          <v-card-title>{{ post.title }}</v-card-title>
+        </div>
+      </v-img>
 
-    <v-card-subtitle class="pb-0">
-      Number 10
-    </v-card-subtitle>
+      <v-card-subtitle>
+        {{ post.eventDate.toDate() }}
+      </v-card-subtitle>
 
-    <v-card-text class="text--primary">
-      {{ post.content }}
-    </v-card-text>
-    <v-card-actions>
-      <v-btn color="orange" text>
-        Share
-      </v-btn>
+      <v-card-text class="text--primary">
+        {{ post.content }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="orange" text>
+          Share
+        </v-btn>
 
-      <v-btn color="orange" text>
-        Explore
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-btn color="orange" text>
+          Explore
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
@@ -34,3 +42,8 @@ export default {
   props: ['post']
 }
 </script>
+
+<style lang="sass" scoped>
+.news-title
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1))
+</style>
