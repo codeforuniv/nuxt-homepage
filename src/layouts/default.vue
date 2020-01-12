bfc1584445bcdd44f7d852eb130fd0466ad5472e
Takahiro Miura <ttmiura225@gmail.com>
2020-01-12 16:51:28 +0900

    Footerを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -38,10 +38,66 @@
       <v-container class="pa-0" fluid>
         <nuxt />
       </v-container>
+      <v-footer padless>
+        <v-card
+          flat
+          tile
+          class="white--text text-center pa-5"
+          style="background: rgb(40,170,170)"
+        >
+          <v-row>
+            <v-col cols="12" md="4" class="text-left">
+              <v-row class="ma-2">Twitter</v-row>
+              <v-row class="justify-center">
+                <a class="twitter-timeline" data-width="400" data-height="300" href="https://twitter.com/codeforuniv?ref_src=twsrc%5Etfw">Tweets by codeforuniv</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="4" class="text-left">
+              <v-row class="ma-2">クイックリンク</v-row>
+              <v-row class="px-10 align-center">
+                <v-list flat color="rgba(0,0,0,0)">
+                  <v-list-item
+                    v-for="(item, i) in items"
+                    :key="i"
+                    :to="item.to"
+                    class="white--text"
+                  >
+                    {{ item.title }}
+                  </v-list-item>
+                </v-list>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="4" class="text-left">
+              <v-row class="ma-2">お問い合わせ</v-row>
+              <v-row class="px-10 align-center">
+                <v-list flat color="rgba(0,0,0,0)">
+                  <v-list-item class="white--text">
+                    Email: code4univ@gmail.com
+                  </v-list-item>
+                  <v-list-item href="https://twitter.com/codeforuniv">
+                    Twitter: @codeforuniv
+                  </v-list-item>
+                  <v-list-item href="https://www.facebook.com/Code-for-University-103516374334392">
+                    Facebook: Code for University
+                  </v-list-item>
+                </v-list>
+              </v-row>
+            </v-col>
+          </v-row>
+
+          <v-card-text class="white--text pt-0">
+            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} — <strong>Code for University.</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
     </v-content>
-    <v-footer>
-      <span>&copy; 2019 Code for University</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -83,14 +139,8 @@ export default {
           title: 'Contact',
           to: '/contact'
         }
-      ],
-      title: 'Vuetify.js'
+      ]
     }
   }
 }
 </script>
-
-<style lang="sass" scoped>
-.clear
-  opacity: 1
-</style>
