79dff1d1f19cd080549ad1fc1bd25bed036fd13d
Takahiro Miura <ttmiura225@gmail.com>
2020-07-20 15:42:46 +0900

    componentフォルダ構成の微修正、不要ファイルの削除

--- src/components/README.md
+++
@@ -1,4 +0,0 @@
-# COMPONENTS
-
-Carousel.vue: トップページの写真,
-NewsCard.vue: MicroCMS で取ってきた情報を Card にして表示
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,29 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar :clipped-left="clipped" class="clear" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-app-bar class="clear" fixed app>
       <a href="/">
         <img src="/logo-wide.png" width="160px" />
       </a>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
       <v-container class="pa-0" fluid>
         <nuxt />
@@ -34,33 +15,12 @@
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
+import Footer from '@/components/common/Footer.vue'
 
 export default {
   components: { Footer },
   data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-home',
-          title: 'Home',
-          to: '/'
-        },
-        {
-          icon: 'mdi-animation',
-          title: 'Apps',
-          to: '/apps'
-        },
-        {
-          icon: 'mdi-file-edit',
-          title: 'Ideathon',
-          to: '/ideas'
-        }
-      ]
-    }
+    return {}
   }
 }
 </script>
